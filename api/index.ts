125e380154860130c1f650329ee719cd8db5e7c1
michelle-currier <mushel@gmail.com>
2025-06-03 18:39:37 -0500

    updates to the files for the deploy

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,28 @@
-const express = require("express");
-const app = express();
+// import express from 'express';
+// import path from 'path';
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+// Serve static files from the 'public' directory
+app.use(express.static(path.join('public')));
+
+
+app.set('/views', path.join('/views'));
+
+// your other routes and middleware
+app.get('/', (req, res) => {
+    res.render('index', {
+
+    });
+});
+
+// app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+export default app;
